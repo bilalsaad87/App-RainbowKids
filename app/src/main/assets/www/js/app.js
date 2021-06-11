193060794391508fefb0856ba8b76bd2a1aca6c7
Bilal Saad <bilalsaad2005@gmail.com>
2021-06-11 21:41:42 +0300

    Android App learning Colors for Kids

---
+++ app/src/main/assets/www/js/app.js
@@ -0,0 +1,54 @@
+  function playW(){
+       var audio = document.getElementById("audioW");
+       audio.play();
+   }  
+
+   function playR(){
+       var audio = document.getElementById("audioR");
+       audio.play();
+   }  
+
+   function playO(){
+       var audio = document.getElementById("audioO");
+       audio.play();
+   }  
+
+   function playY(){
+       var audio = document.getElementById("audioY");
+       audio.play();
+   }  
+
+   function playG(){
+       var audio = document.getElementById("audioG");
+       audio.play();
+   }  
+
+   function playBl(){
+       var audio = document.getElementById("audioBl");
+       audio.play();
+   }  
+
+   function playG(){
+       var audio = document.getElementById("audioG");
+       audio.play();
+   }  
+
+   function playI(){
+       var audio = document.getElementById("audioI");
+       audio.play();
+   }  
+
+   function playI(){
+       var audio = document.getElementById("audioI");
+       audio.play();
+   }  
+
+   function playV(){
+       var audio = document.getElementById("audioV");
+       audio.play();
+   }  
+
+   function playB(){
+       var audio = document.getElementById("audioB");
+       audio.play();
+   }  
